96e1541bd520dfc89f54b5c9462b35ea9b2f53bd
Norah Taylor <emt368@gmail.com>
2022-04-26 17:07:01 -0400

    commit tree 3f7a14fca7836edf80d14d5bff78b9d3ec484ed8

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = arrayOfDrivers => {
+    return arrayOfDrivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = arrayOfDrivers => {
+    return arrayOfDrivers.slice(-2) 
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers] 
+
+function createFareMultiplier(x) {
+    return function(fare) {
+        return fare * x
+    }
+}
+//const createFareMultiplier= x => fare => (x * fare )
+
+const fareDoubler = createFareMultiplier(2) 
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(arrayOfDrivers, returnSomething) {
+   return returnSomething(arrayOfDrivers)
+}
+    
